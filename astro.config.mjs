e2114a7fe608a1eb5ae5d343bf630d1983ccd1c9
Tom Eaves <hello@tomeaves.dev>
2026-06-16 18:53:33 +0100

    GitHub Pages Setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,8 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import icon from 'astro-icon';
 
-// https://astro.build/config
 export default defineConfig({
+  site: 'https://tomeaves.dev',
   integrations: [mdx(), icon()],
   markdown: {
     shikiConfig: {
